3f8af775b6abbf0a6e97c5e4de61ead287e54044
anupama-gupta <anupama.gupta01@gmail.com>
2016-07-15 11:03:38 +0200

    Update demo.sh

--- demo.sh
+++ demo.sh
@@ -17,35 +17,35 @@ count_contexts=10000
 context_window=5
 processes=8
 
-printf "\nBuilding vocab"
+printf "\nBuilding vocab\n"
 python ./src/vocab_count.py $corpus_path --unigrams $count_unigrams --bigrams $count_bigrams --contexts $count_contexts --nproc $processes
 
-printf "\nBuilding cooccurence counts"
+printf "\nBuilding cooccurence counts\n"
 python ./src/cooccur.py $corpus_path ./dict/contexts_vocab.txt --unigrams ./dict/unigrams_vocab.txt --bigrams ./dict/bigrams_vocab.txt --nproc $processes
 
-printf "\nCreating unigrams semantic space : Main space"
+printf "\nCreating unigrams semantic space : Main space\n"
 python ./src/semantic_space.py unigram_space ./dict/contexts_vocab.txt ./dict/unigrams_vocab.txt ./dict/unigrams_cooccur.txt
 
-printf "\nCreating bigrams semantic space : Peripheral space" 
+printf "\nCreating bigrams semantic space : Peripheral space\n" 
 python ./src/semantic_space.py bigram_space ./dict/contexts_vocab.txt ./space/unigrams_space.pkl ./dict/bigrams_cooccur.txt
 
-printf "\nLearn ADJ matrices" 
+printf "\nLearn ADJ matrices\n" 
 python ./src/lex_functions.py learn_ADJ ./space/unigrams_space.pkl ./space/bigrams_space.pkl
 
-printf "\nLearn TENSOR matrix"
+printf "\nLearn TENSOR matrix\n"
 python ./src/lex_functions.py learn_TENSOR ./space/unigrams_space.pkl ./space/bigrams_space.pkl
 
-printf "\nCompose new compound vectors space using ADJ matrices"
+printf "\nCompose new compound vectors space using ADJ matrices\n"
 python ./src/lex_functions.py ADJ_space ./space/unigrams_space.pkl ./space/bigrams_space.pkl ./matrices/ADJ_matrices.pkl
 
-printf "\nCompose new compound vectors space using TENSOR matrix"
+printf "\nCompose new compound vectors space using TENSOR matrix\n"
 python ./src/lex_functions.py TENSOR_space ./space/unigrams_space.pkl ./space/bigrams_space.pkl ./matrices/TENSOR_matrix.pkl 
 
-printf "\nNearest neighbours of a compound (predicted using ADJ matrix )"
+printf "\nNearest neighbours of a compound (predicted using ADJ matrix )\n"
 python ./src/lex_functions.py neighbours_ADJ ./space/unigrams_space.pkl small_town ./composed_space/composed_space_ADJ.pkl ./matrices/ADJ_matrices.pkl
 
-printf "\nNearest neighbours of a compound (predicted using TENSOR matrix )"
+printf "\nNearest neighbours of a compound (predicted using TENSOR matrix )\n"
 python ./src/lex_functions.py neighbours_TENSOR ./space/unigrams_space.pkl small_town ./composed_space/composed_space_TENSOR.pkl ./matrices/TENSOR_matrix.pkl
 
-printf "\nNearest neighbours of compound vectors (obtained from bigram_space)"
+printf "\nNearest neighbours of compound vectors (obtained from bigram_space)\n"
 python ./src/lex_functions.py neighbours_bigrams small_town ./space/bigrams_space.pkl ./space/bigrams_space.pkl
